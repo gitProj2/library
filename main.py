9d9730e3bd6e3f12ecb898110f288bbc6fec0483
정원준 <dnjswnsakdmf@gmail.com>
2020-10-19 19:20:46 +0900

    10/19 commit

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import mariadb
 import sys
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
 
 def get_conn():
     conn = mariadb.connect(
-        user="test",
+        user="root",
         password="password",
-        host="10.5.2.20",
+        host="193.122.100.83",
         port=13306,
         database="LIBRARY"
     )
@@ -18,6 +17,9 @@ def get_conn():
 
 @app.route('/')
 def main():
+    session.clear();
+    session['id'] = "wonjun7"
+    session['number'] = 1
     return render_template("/main.html")
 
 @app.route('/sign_in')
@@ -50,23 +52,106 @@ def write_doc():
 
 @app.route("/books")
 def books():
-    sql = "SELECT NAME, IMG, LOAN FROM BOOK;"
+    catagory_number = request.args.get("catagory_number")
+    title = request.args.get("title")
+    number = session["number"]
 
     try:
+        sql = "SELECT NUMBER, NAME FROM CATAGORY_BOOK ORDER BY NUMBER;"
+
         conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+
+        aside = """
+            <li><h4><a href="/books">전체보기</a></h4></li>"""
+
+        for (NUMBER, NAME) in cur:
+            aside += """
+            <li><a href="/books?catagory_number='{0}'">{1}</a></li>""".format(NUMBER, NAME)
+
+        sql = """
+        SELECT B.NUMBER, B.NAME, B.IMG, B.LOAN, R.MEMBER_NUMBER FROM RENTER_RECORD R
+        JOIN BOOK B
+        ON B.NUMBER = R.BOOK_NUMBER 
+        AND R.LOAN = 'N'"""
+
+        sql2 = """
+        UNION ALL
+        SELECT B.NUMBER, B.NAME, B.IMG, B.LOAN ,0 AS MEMBER_NUMBER FROM BOOK B 
+        WHERE LOAN != 'N'
+        """
+
+        if catagory_number is not None:
+            sql += "AND CATAGORY_NUMBER = "
+            sql += catagory_number + " "
+            sql2 += "AND CATAGORY_NUMBER = "
+            sql2 += catagory_number + " "
+
+        if title is not None:
+            sql += "AND NAME LIKE '%{0}%' ".format(title)
+            sql2 += "AND NAME LIKE '%{0}%' ".format(title)
+
+
+        sql2 += "ORDER BY NUMBER ASC "
+
+
+        sql += sql2
+        print(sql)
         cur = conn.cursor()
         cur.execute(sql)
 
         result = ""
 
-        for (NAME, IMG, LOAN) in cur:
+        for (NUMBER, NAME, IMG, LOAN, MEMBER_NUMBER) in cur:
             result += """
-                <div class="book">
-                    <input type="image" src="{1}" alt="책" width="100px" height="160px">
-                    <span>{0}</span>
-                    <span>{2}</span>
-                </div>
-                """.format(NAME, IMG, LOAN)
+                <div class="book" id="book_{0}">
+                    <input type="image" src="{2}" alt="책" width="100px" height="160px">
+                    <span>{1}</span>""".format(NUMBER, NAME, IMG)
+            if MEMBER_NUMBER == number:
+                if LOAN != 'Y':
+                    result += """
+                        <button type="button" id="book_borrow_{0}" onclick="return_book('{0}',this)">반납</button>
+                    </div>""".format(NUMBER)
+            else :
+                if LOAN == 'Y':
+                    result += """
+                        <button type="button" id="book_borrow_{0}" onclick="borrow_book('{0}',this)">대여</button>
+                    </div>""".format(NUMBER)
+                else:
+                    result += """
+                        <span id="book_borrow_{0}">대여 중</span>
+                    </div>""".format(NUMBER)
+
+    except mariadb.Error as e:
+        print(e)
+        sys.exit(1)
+    except TypeError as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+    return render_template("/books.html", tag=aside, content=result)
+
+@app.route("/book_borrow")
+def book_borrow():
+    id = session["id"]
+    book_number = request.args.get("book_number")
+
+    try:
+        sql = "INSERT INTO RENTER_RECORD (MEMBER_NUMBER, BOOK_NUMBER, DATE, RETURN_DATE, LOAN) VALUES((SELECT NUMBER FROM MEMBER WHERE ID='{0}'), {1}, NOW(), NULL, 'N')".format(id, book_number)
+
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+
+        sql = "UPDATE BOOK SET LOAN = 'N' WHERE NUMBER = {0}".format(book_number)
+
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
 
     except mariadb.Error as e:
         result = "사용자 없음."
@@ -77,9 +162,41 @@ def books():
         if conn:
             conn.close()
 
-    return render_template("/books.html", content=result)
+    return "대여 성공!"
+
+@app.route("/book_return")
+def return_book():
+    id = session["id"]
+    number = session["number"]
+    book_number = request.args.get("book_number")
+
+    try:
+        sql = "UPDATE BOOK SET LOAN = 'Y' WHERE NUMBER = {0}".format(book_number)
+
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+
+        sql = "UPDATE RENTER_RECORD SET LOAN = 'Y', RETURN_DATE = NOW() WHERE MEMBER_NUMBER = {0} AND BOOK_NUMBER = {1} ".format(number, book_number)
+
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+
+    except mariadb.Error as e:
+        result = "사용자 없음."
+        sys.exit(1)
+    except TypeError as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+    return "반납 성공!!"
 
 
 if __name__ == "__main__":
+    app.secret_key = 'app secret key'
     app.run(host='0.0.0.0')
 
