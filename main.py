9d133f7debafdcd9f9aded44dee2f6bf5ef3215b
정원준 <dnjswnsakdmf@gmail.com>
2020-10-21 12:08:43 +0900

    library version 2.0

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import mariadb
-import sys
+import sys, datetime
 from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
@@ -18,40 +18,512 @@ def get_conn():
 @app.route('/')
 def main():
     session.clear()
-    session['id'] = "wonjun7"
-    session['number'] = 1
     return render_template("/main.html")
 
+#로그인 화면
 @app.route('/sign_in')
 def sign_in():
     return render_template("/sign_in.html")
 
+#로그인 화면에서 입력한 ID와 PW 체크 (DB에 있는지 여부 확인)
+@app.route('/sign_in', methods=['POST'])
+def check_id():
+    insert_id = request.form['id']
+    insert_pw = request.form['pw']
+    login_flag = False
+
+    result =""
+
+    sql = "SELECT ID, PW, NUMBER FROM MEMBER WHERE ID = '{}'".format(insert_id)
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+
+        for (ID, PW, NUMBER) in cur:
+            result = "{0},{1}".format(ID,PW)
+
+            # 입력한 ID와 PW가 DB와 일치하는 경우
+            if ID == insert_id and PW == insert_pw :
+                session.clear()
+                session['id'] = request.form['id']
+                session['number'] = NUMBER
+                login_flag = True
+                break
+
+    except mariadb.Error as e:
+        print("ERR: {}".format(e))
+        sys.exit(1)
+    except TypeError as e:
+        result = ""
+    if conn:
+        conn.close()
+        # 입력한 ID나 PW 둘 중 하나라도 DB와 일치하지 않은 경우
+        result = """
+        <script>
+        alert("아이디 또는 패스워드를 확인 하세요.");
+        </script>
+        """
+    if login_flag: return render_template('/main.html')
+    else: return render_template('/sign_in.html', content=result)
+
+#회원가입화면
 @app.route('/sign_up')
 def sign_up():
     return render_template("/sign_up.html")
 
+#회원가입 정보를 DB 입력
+@app.route('/sign_up', methods=['POST'])
+def member_info_insert():
+    new_id = request.form["id"]
+    new_pw = request.form["pw"]
+    new_name = request.form["name"]
+    new_gender = request.form["gender"]
+    new_birthday = request.form["birthday"]
+    new_phone = request.form["phone"]
+    new_email = request.form["email"]
+
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        sql = "INSERT INTO MEMBER(id, pw, name, gender, birthday, phone, email) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}')".format(new_id, new_pw, new_name, new_gender, new_birthday, new_phone, new_email)
+        print(sql)
+        cur.execute(sql)
+        conn.commit()
+
+    except mariadb.Error as e:
+        print(e)
+        sys.exit(1)
+    except TypeError as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+    return render_template('/main.html')
+
+# 로그인 한 경우 회원정보 불러오기
 @app.route('/member_info')
 def member_info():
-    return render_template("/member_info.html")
+    result = ""
+
+    # 로그인 한 값이 있는 경우 DB에서 해당 정보를 불러오고, 없는 경우 로그인 알림창 뜸.
+    if 'number' in session:
+        r_num = session['number']
+    else:
+        result = """
+        <script>
+        alert("먼저 로그인 하세요.");
+        </script>
+        """
+        return render_template("/main.html", content=result)
+
+    sql = "SELECT NUMBER, ID, PW, PHONE, EMAIL, GENDER, NAME, BIRTHDAY FROM MEMBER WHERE NUMBER = {}".format(r_num)
+
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+        
+        result = ""
+        for (ID, PW, NAME, GENDER, BIRTHDAY, PHONE, EMAIL, NUMBER) in cur:
+            # < input type = "hidden" value = "{0}" >
+
+            result = """      
+            <article>
+            <div>            
+            <ul class=\"box1\">
+                <li>아이디 &emsp;&emsp;<input type="text" name="id" readonly text-align="left" height="40" value="{1}" class="input_box"></li>
+                <li>비밀번호 &emsp;<input type="password" name="pw" text-align="left" height="40" value="{2}" class="input_box"></li>
+            </ul>
+            &nbsp;
+        </div>
+        <div>
+            <ul class=\"box2\">
+                <li>이름 &emsp;&emsp;&emsp;<input type="text" name="name" height="40" value="{6}" class="input_box" ></li>
+                <li>성별 &emsp;&emsp;&emsp;<input type="text" name="gender" height="40" value="{5}" class="input_box" ></li>
+                <li>생년월일 &emsp;<input type="text" name="birthday" height="40" value="{7}" class="input_box" ></li>
+                <li>연락처 &emsp;&emsp;<input type="text" name="phone" height="40" value="{3}" class="input_box"></li>
+                <li>이메일 &emsp;&emsp;<input type="text" name="email" height="40" value="{4}" class="input_box"></li>
+            </ul>
+        </div>""".format(ID, PW, NAME, GENDER, BIRTHDAY, PHONE, EMAIL, NUMBER)
+
+    except mariadb.Error as e:
+        print(e)
+        sys.exit(1)
+    except TypeError as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+    return render_template("/member_info.html", content=result)
+
+# 만일 불러온 회원정보에서 수정사항이 있는 경우 수정된 회원정보 DB입력 / ID는 수정불가
+@app.route('/member_info', methods=['POST'])
+def member_info_modify():
+    id = request.form["id"]
+    pw = request.form["pw"]
+    name = request.form["name"]
+    gender = request.form["gender"]
+    birth= request.form["birthday"]
+    phone = request.form["phone"]
+    email = request.form["email"]
+    sql = "UPDATE MEMBER SET pw='{0}',name='{1}',gender='{2}',birthday='{3}',phone='{4}',email='{5}' WHERE id='{6}'".format(
+        pw, name, gender, birth, phone, email, id)
+
+    print(sql)
+
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+
+    except mariadb.Error as e:
+        print(e)
+    except TypeError as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+    return render_template("/main.html")
+
 
 @app.route('/master/master')
-def master():
-    return render_template("/master/master.html")
+def master_m():
+    sql = "SELECT NAME, ID, PHONE, EMAIL FROM MEMBER;"
+
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+
+        result = ""
+
+        for (NAME, ID, PHONE, EMAIL) in cur:
+            result += """
+                <tbody>
+                    <tr>
+                        <td>{0}</td>
+                        <td>{1}</td>
+                        <td>{2}</td>
+                        <td>{3}</td>
+                    </tr>
+                <tbody>
+                """.format(NAME, ID, PHONE, EMAIL)
+    except mariadb.Error as e:
+        print(e)
+        result = "사용자 없음."
+        sys.exit(1)
+    except TypeError as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+    return render_template("/master/master.html", content=result)
+
+
+@app.route('/master/community')
+def master_c():
+    sql = """
+        SELECT m.ID, p.NUMBER, p.title, p.date
+        FROM LIBRARY.POST as p
+        LEFT join LIBRARY.MEMBER as m
+        on p.MEMBER_NUMBER = m.NUMBER ORDER by p.NUMBER desc;
+        """
+    result = ""
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+
+        result += """<table>
+                        <thead>
+                            <tr>
+                                <th>번호</th>
+                                <th>제목</th>
+                                <th>글쓴이</th>
+                                <th>작성일시</th>
+                            </tr>
+                        </thead>
+                    """
+        for_rotation_counting = 0
+        for (id, number, title, date) in cur:
+            for_rotation_counting += 1
+            result += """
+                        <tr>
+                            <th>{1}</th>
+                            <th><a href="/community/watch_doc?p.number={4}">{2}</a></th> 
+                            <th>{0}</th>
+                            <th>{3}</th>
+                        </tr>
+
+                        """.format(id, number, title, date, number)
+
+        result += "</table>"
+    except mariadb.Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+    return render_template("/master/community.html", content=result, content1=for_rotation_counting)
+
+
+@app.route('/master/books')
+def master_b():
+    sql = "SELECT NAME, IMG, LOAN, CONTENTS FROM BOOK;"
+
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+
+        result = ""
+
+        for (NAME, IMG, LOAN, CONTENTS) in cur:
+            result += """
+                <div class="book">
+                    <input type="image" src="{1}" alt="책" width="100px" height="160px" style="margin-right:15px;">
+                    <span style="font-size:22px; position:absolute;">{3}</span><br>
+                    <span style="font-size:18px; color:blue;">{0}</span>
+                    <span style=font-size:25px;> / <span>
+                    <span style=font-size:18px;>대여여부 : </span>
+                    <span style=color:red;>{2}</span><br><br><br>
+                </div>
+                """.format(NAME, IMG, LOAN, CONTENTS)
+    except mariadb.Error as e:
+        result = "사용자 없음."
+        sys.exit(1)
+    except TypeError as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+    return render_template("/master/books.html", content=result)
+
 
 @app.route('/community/board_home')
+## 글 작성 버튼 클릭하면 회원여부 확인 후 글 작성 페이지로 이동, 회원이 아니면 경고 메시지 띄우기- 기능 구현 전
 def board_home():
-    return render_template("/community/board_home.html")
+# 데이터베이스에서 아래 값들을 불러와 보드홈에서 내림차순 테이블 구현
+    sql= """
+        SELECT m.ID, p.NUMBER, p.title, p.date
+        FROM LIBRARY.POST as p
+        LEFT join LIBRARY.MEMBER as m
+        on p.MEMBER_NUMBER = m.NUMBER ORDER by p.NUMBER desc;
+        """
+    result = ""
 
-@app.route('/community/watch_doc')
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+
+        result += """<table>
+                        <thead>
+                            <tr>
+                                <th>번호</th>
+                                <th>제목</th>
+                                <th>글쓴이</th>
+                                <th>작성일시</th>
+                            </tr>
+                        </thead>
+                    """
+        for_rotation_counting = 0 # for 문이 회전하는 횟수를 계산해 게시글 수 확인 후 변수에 담아 테이블과 함께 게시판 html에 전달
+        for (id, number, title, date) in cur:
+            for_rotation_counting +=1
+            result += """
+                        <tr>
+                            <th>{1}</th>
+                            <th><a href="/community/watch_doc?p.number={4}">{2}</a></th> 
+                            <th>{0}</th>
+                            <th>{3}</th>
+                        </tr>
+                           
+                        """.format(id, number, title, date, number)
+            # 글 제목에 링크를 걸어 해당 글을 화면 이동
+
+        result += "</table>"
+    except mariadb.Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+    return render_template("/community/board_home.html", content=result, content1=for_rotation_counting)
+
+@app.route('/community/board_home_search', methods = ["GET"]) # 게시판 홈에서 검색어를 GET방식으로 전달 받아 DB에서 값 수신
+def search_doc():
+    search_text_val = request.args.get("search_text")
+
+    sql = """
+        SELECT p.NUMBER, p.TITLE, m.ID, p.DATE 
+        FROM LIBRARY.POST as p left join LIBRARY.MEMBER as m
+        on m.NUMBER = p.MEMBER_NUMBER
+        where p.TITLE LIKE '%{}%'
+        order by p.NUMBER DESC;
+        """.format(search_text_val)
+    result = ""
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+
+        result += """
+            <table>
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>글쓴이</th>
+                        <th>작성일시</th>
+                    </tr>
+                </thead> """
+
+        for_rotation_counting = 0
+        for (number, title, id, date) in cur:
+            for_rotation_counting +=1
+            result += """
+                    <tbody>
+                        <tr>
+                            <td>{0}</td>
+                            <td><a href="/community/watch_doc?p.number={4}">{1}</a></td>
+                            <td>{2}</td>
+                            <td>{3}</td>
+                        </tr>
+                    </tbody>
+                """.format(number, title, id, date, number)
+        result += "</table>"
+    except mariadb.Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+    return render_template("/community/board_home_search.html", content = result, content1 = for_rotation_counting)
+    #DB에서 가져온 값을 자동 생성 표에 넣고 이를 담은 변수와 for문의 회전 횟수를 담은 변수를 함께 위 경로에 렌더링
+
+@app.route('/community/watch_doc', methods = ["GET"])
+#게시판 홈에서 사용자가 클릭한 글 제목을 GET 방식으로 watch_doc에서 전달 받아 DB 연산 진행
 def watch_doc():
-    return render_template("/community/watch_doc.html")
+    post_number = request.args.get("p.number")
+    result = ""
+    try:
+        sql = """
+            SELECT p.title, p.CONTENTS, p.post_file, m.id, p.date,
+            p.MODIFY_DATE, p.number FROM LIBRARY.POST as p
+            left join LIBRARY.MEMBER as m on p.MEMBER_NUMBER = m.NUMBER
+            WHERE p.NUMBER = {};
+            """.format(post_number)
 
-@app.route('/community/write_doc')
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+
+
+        for (title, contents, file, id, date, modify_date,p_number) in cur:
+            if modify_date == None:
+                modify_date = "수정 이력 없음"
+
+                result += """
+                        <h3>{0}</h3>
+                        <div class="container">
+                            <p>{1}</p>
+                            <a href ="{2}" download><input type="button" value="첨부파일 다운로드"></a>                              
+                            <input class="float-right" type="button" value="삭제">
+                            <input class="float-right" type="button" value="수정" onclick="location.href='/community/amend_doc?p_number={6}'">
+                            <button class="float-right" disabled><span style="color: white;">작성자 : {3} 작성일시 : {4} 최종 수정 : {5}</span></button>    
+                        </div>
+                        """.format(title, contents, file, id, date, modify_date, p_number)
+            else:
+                result +=  """
+                            <h3>{0}</h3>
+                            <div class="container">
+                                <p>{1}</p>
+                                <a href ="{2}" download><input type="button" value="첨부파일 다운로드"></a>                            
+                                <input class="float-right" type="button" value="삭제">
+                                <input class="float-right" type="button" value="수정" onclick="location.href='/community/amend_doc?p_number={6}'">
+                                <button class="float-right" disabled><span style="color: white;">작성자 :  {3} 작성일시 : {4} 최종 수정 : {5}</span></button>  
+                            </div>
+                        """.format(title, contents, file, id, date, modify_date, p_number)
+    except mariadb.Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+    return render_template("/community/watch_doc.html", content= result)
+    #연산한 결과를 watch_doc.html에서 전달 받아 이를 출력
+
+
+@app.route('/community/write_doc')#사용자가 글 작성 메뉴를 클릭하면 이동되는 화면
 def write_doc():
-    return render_template("/community/write_doc.html")
+    author = session['id']
+    return render_template("/community/write_doc.html", author = author)
 
 
-@app.route("/books")
+# @app.route('/community/watch_doc')
+# def add_comment():
+#     author = session['id']
+#     return render_template("/community/watch_doc.html", author = author)
+
+@app.route('/community/amend_doc', methods = ['GET'])
+# 사용자가 수정을 클릭하면 실행되는 함수, 글 번호를 GET방식으로 전달받는다.
+def amend_doc():
+    author = session['id']
+    p_number = request.args.get("p_number")
+
+    # 해당 경로에서 각기 다른 인자값을 전달 받아 DB 연산을 수행하는 용례
+
+    # tag = request.args.get("tag")
+    # ajax = request.args.get("ajax")
+    # if tag:
+    #     ~~~
+    # elif ajax:
+    #     ~~~~
+
+    sql = """ 
+        SELECT p.title, p.CONTENTS, p.post_file, p.NUMBER FROM LIBRARY.POST as p
+        WHERE p.NUMBER = {};
+        """.format(p_number)
+    result = ""
+    #수정에 필요한 모든 값들을 쿼리문으로 불러옴.
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+
+
+        for (title, contents, file, p_number) in cur:
+            result += """ 
+                        <form action="/community/watch_doc?amend_p_number={3}" method="POST">
+                            <p>제목 : </p>
+                            <input type="text" value="{0}" name= "amend_title">
+                            <br>
+                            <textarea name="amend_contents">{1}</textarea>
+                            <br>
+                            <div>
+                                <p>파일첨부 :</p><input type="file" onclick="" name="amend_file" value="{2}">
+                                <input type="submit" value="저장" onclick="">
+                            </div>
+                        </form>
+                    """.format(title, contents, file, p_number)
+    except mariadb.Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+    return render_template("/community/watch_doc_amend.html", author = author, content_amend = result)
+    #실행결과를 watch_doc_amend.html에 변수에 담아 전달
+
+
+@app.route('/books')
 def books():
     ##################################################################################
     #   2020-10-20 정원준                                                             #
@@ -147,7 +619,7 @@ def books():
 
     return render_template("/books.html", tag=aside, content=result)
 
-@app.route("/book_borrow")
+@app.route('/book_borrow')
 def book_borrow():
     ##################################################################################
     #   2020-10-20 정원준                                                             #
@@ -189,7 +661,7 @@ def book_borrow():
 
     return "대여 성공!"
 
-@app.route("/book_return")
+@app.route('/book_return')
 def return_book():
     ##################################################################################
     #   2020-10-20 정원준                                                             #
